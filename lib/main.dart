525e366132e42ea451af6321c78b6d935ab0778b
cantuario2 <cantuario2@hotmail.com>
2023-11-26 01:02:42 -0300

    20231126_initial_commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pieces_cheatsheet/pages/bottom_app_bar.dart';
+import 'package:flutter_pieces_cheatsheet/pages/home.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Cantuario2 Flutter Cheatsheet",
+      initialRoute: "/",
+      routes: {
+        '/': (_) => const Home(),
+        '/bottomappbar': (_) => const BottomAppBarDemo(),
+      },
+    ),
+  );
+}
